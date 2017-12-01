186ea91c8360d4aad225ceaf80bb432f7bedd5a2
shunwuyu <shunwu2001@163.com>
2017-12-01 09:08:25 +0800

    airbnb

---
+++ es6/loop.js
@@ -0,0 +1,9 @@
+var myArr = [1,2,3];
+for(var index = 0, len = myArr.length; index < len; index++) {
+  console.log(myArr[index]);
+}
+
+// forEach
+myArray.forEach(function(value) {
+  console.log(value);
+})
